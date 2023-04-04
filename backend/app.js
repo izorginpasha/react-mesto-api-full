e49204b1243e4a1f53ddd20d9195556f56ca0b8e
izorginpasha <izorgin.pasha@yandex.ru>
2023-04-04 13:57:26 +0500

    clon

---
+++ backend/app.js
@@ -0,0 +1,97 @@
+const bodyParser = require("body-parser");
+const express = require("express"); //
+const mongoose = require("mongoose"); //
+const routerUsers = require("./routes/routerUsers"); //
+const routerCards = require("./routes/routerCards"); //
+const auth = require("./middlewares/auth");
+const cors = require('cors');
+
+const { Console } = require("console");
+const { errors } = require("celebrate");
+const { login, createUser } = require("./controllers/users");
+const NotFoundError = require("./erors/NotFoundError");
+const path = require("path");
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+// Слушаем 3000 порт
+const { celebrate, Joi, Segments } = require("celebrate");
+const shemaUser = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().min(2).max(30).default("Жак-Ив Кусто"), // имя пользователя
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    about: Joi.string().min(2).max(30).default("Исследователь"), // информация о пользователе
+    avatar: Joi.string()
+      .pattern(new RegExp("(www|http:|https:)+S*"))
+      .default(
+        "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png"
+      ), // ссылка на аватарку
+  }),
+});
+require('dotenv').config({ path: './.env' })
+
+const { NODE_ENV, key } = process.env;
+const  PORT = 3000  //порт
+const app = express(); //создаем сервер
+app.use(cors())//защита корс
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");// все запросы
+
+  next();
+});
+// подключаем мидлвары, роуты и всё остальное...
+app.use(bodyParser.json());
+// подключаем главный роутер приложения на /api
+
+// роуты, не требующие авторизации,
+// раздаём папку с собранным фронтендом
+app.use(express.static(path.join(__dirname, "public")));
+// раняем сервер
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+
+app.post("/signup", shemaUser, createUser);
+app.post("/signin", shemaUser, login);
+
+
+// авторизация
+app.use("/api/users", auth, routerUsers); //роуты на пути user
+app.use("/api/cards", auth, routerCards); //роуты на пути Cards
+
+app.get("*", function (req, res) {
+  res.redirect("/");
+});
+
+
+try {
+  app.all("*", function (req, res) {
+    //обработка неправильных путей
+    console.log("404 handler..");
+    throw new NotFoundError("неверныи путь");
+
+    // res.status(404).json({ message: "Произошла ошибка" });
+  });
+} catch {
+  next(err);
+}
+
+try {
+  mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {}, () => {
+    console.log("Connected MongoDB");
+    app.listen(PORT, () => {
+      //запуск сервера
+      // Если всё работает, консоль покажет, какой порт приложение слушает
+      console.log(`App listening on port ${PORT}`);
+    });
+  });
+} catch {
+  console.log("not connected MongoDB");
+}
+app.use(requestLogger); // подключаем логгер запросов
+app.use(errors());// обработчик ошибок celebrate
+app.use((err, req, res, next) => {// централизованный обработчик ошибок
+  res.status(err.statusCode).send({ message: err.message });
+});
---
+++ backend/middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { ERROR_AUTH } = require("../utils/constants");
+const AuthErors = require("../erors/AuthErors");
+require('dotenv').config({ path: './.env' })
+const {  key } = process.env;
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return next(new AuthErors("Передан неверный логин или пароль"));
+  }
+
+  const token = authorization.replace("Bearer ", "");
+  let payload;
+
+  try {
+    payload = jwt.verify(token, key);
+  } catch (err) {
+    console.log(err);
+    return next(new AuthErors("Передан неверный логин или пароль"));
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  next(); // пропускаем запрос дальше
+};
+
